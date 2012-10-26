5d73b3ca17f4aed52b55c30a34a0eb3c678563a5
Stephen Connolly <stephen.alan.connolly@gmail.com>
2012-10-26 15:09:03 +0100

    Forgot to add the license header, and some javadocs

--- src/main/java/com/cloudbees/jenkins/plugins/sshagent/RemoteAgentFactory.java
+++ src/main/java/com/cloudbees/jenkins/plugins/sshagent/RemoteAgentFactory.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2012, CloudBees, Inc., Stephen Connolly.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.plugins.sshagent;
 
 import hudson.ExtensionPoint;
@@ -5,13 +29,32 @@ import hudson.Launcher;
 import hudson.model.TaskListener;
 
 /**
- * @author stephenc
- * @since 26/10/2012 14:32
+ * Extension point for ssh-agent providers.
  */
 public abstract class RemoteAgentFactory implements ExtensionPoint {
+    /**
+     * The display name of the factory.
+     *
+     * @return The display name of the factory.
+     */
     public abstract String getDisplayName();
 
+    /**
+     * Checks if the supplied launcher is supported by this factory.
+     *
+     * @param launcher the launcher on which the factory would be asked to start a ssh-agent.
+     * @param listener a listener in case any user diagnostics are to be printed.
+     * @return {@code false} if the factory does not want to try and start an ssh-agent on the launcher.
+     */
     public abstract boolean isSupported(Launcher launcher, TaskListener listener);
 
+    /**
+     * Start a ssh-agent on the specified launcher.
+     *
+     * @param launcher the launcher on which to start a ssh-agent.
+     * @param listener a listener for any diagnostics.
+     * @return the agent.
+     * @throws Throwable if the agent cannot be started.
+     */
     public abstract RemoteAgent start(Launcher launcher, TaskListener listener) throws Throwable;
 }
--- src/main/java/com/cloudbees/jenkins/plugins/sshagent/mina/MinaRemoteAgentFactory.java
+++ src/main/java/com/cloudbees/jenkins/plugins/sshagent/mina/MinaRemoteAgentFactory.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2012, CloudBees, Inc., Stephen Connolly.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.cloudbees.jenkins.plugins.sshagent.mina;
 
 import com.cloudbees.jenkins.plugins.sshagent.RemoteAgent;
@@ -9,17 +33,22 @@ import hudson.remoting.Callable;
 import org.apache.tomcat.jni.Library;
 
 /**
- * @author stephenc
- * @since 26/10/2012 14:35
+ * A factory that uses the Apache Mina/SSH library support to (semi-)natively provide a ssh-agent implementation
  */
 @Extension
 public class MinaRemoteAgentFactory extends RemoteAgentFactory {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getDisplayName() {
         return "Java/JNI ssh-agent";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isSupported(Launcher launcher, final TaskListener listener) {
         try {
@@ -29,6 +58,9 @@ public class MinaRemoteAgentFactory extends RemoteAgentFactory {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public RemoteAgent start(Launcher launcher, final TaskListener listener) throws Throwable {
         return launcher.getChannel().call(new MinaRemoteAgentStarter(listener));
