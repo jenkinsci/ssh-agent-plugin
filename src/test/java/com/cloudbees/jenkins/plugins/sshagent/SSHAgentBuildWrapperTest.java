4822a11730bd43898a8eb6f3f510cc1daac11de5
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-12-07 09:41:44 +0000

    Add an important test case

--- src/test/java/com/cloudbees/jenkins/plugins/sshagent/SSHAgentBuildWrapperTest.java
+++ src/test/java/com/cloudbees/jenkins/plugins/sshagent/SSHAgentBuildWrapperTest.java
@@ -52,6 +52,37 @@ public class SSHAgentBuildWrapperTest extends SSHAgentBase {
     }
 
     @Test
+    public void sshAgentDoesNotDieAfterFirstUse() throws Exception {
+        startMockSSHServer();
+
+        List<String> credentialIds = new ArrayList<String>();
+        credentialIds.add(CREDENTIAL_ID);
+
+        SSHUserPrivateKey key = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL, credentialIds.get(0), "cloudbees",
+                new BasicSSHUserPrivateKey.DirectEntryPrivateKeySource(getPrivateKey()), "cloudbees", "test");
+        SystemCredentialsProvider.getInstance().getCredentials().add(key);
+        SystemCredentialsProvider.getInstance().save();
+
+        FreeStyleProject job = r.createFreeStyleProject();
+
+        SSHAgentBuildWrapper sshAgent = new SSHAgentBuildWrapper(credentialIds, false);
+        job.getBuildWrappersList().add(sshAgent);
+
+        Shell shell = new Shell("ssh -o StrictHostKeyChecking=no -p " + getAssignedPort() + " -v -l cloudbees " + SSH_SERVER_HOST);
+        job.getBuildersList().add(shell);
+
+        shell = new Shell("ssh-add -l");
+        job.getBuildersList().add(shell);
+
+        shell = new Shell("ssh -o StrictHostKeyChecking=no -p " + getAssignedPort() + " -v -l cloudbees " + SSH_SERVER_HOST);
+        job.getBuildersList().add(shell);
+
+        r.assertBuildStatusSuccess(job.scheduleBuild2(0));
+
+        stopMockSSHServer();
+    }
+
+    @Test
     public void sshAgentUnavailable() throws Exception {
         startMockSSHServer();
 
